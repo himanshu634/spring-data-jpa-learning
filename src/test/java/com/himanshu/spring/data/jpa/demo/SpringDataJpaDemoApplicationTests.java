432df0d25bab09ac99cc515189005f276a49c754
Himashu Mendapara <himanshumendapra@gmail.com>
2022-07-20 14:51:45 +0530

    Initial commit

---
+++ src/test/java/com/himanshu/spring/data/jpa/demo/SpringDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.spring.data.jpa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
